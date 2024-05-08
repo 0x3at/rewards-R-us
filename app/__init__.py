cd3e573d2562f19b4629b1d6a4ccef12ca6bc3cf
Bowtiedwave <119373192+0x3at@users.noreply.github.com>
2024-05-08 01:56:11 -0500

    create project structure

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,14 @@
 from flask import Flask
+from .extensions import database
 
-app = Flask(__name__)
+
+def create_app():
+    app = Flask(__name__)
+
+    app.config["SECRET_KEY"] = "secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    app.config["DEBUG"] = True
+
+    database.register_extension(app)
+
+    return app
